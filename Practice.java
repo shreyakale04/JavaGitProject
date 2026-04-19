104d35fc4fb82d5f4ffd53be8e83726296a61e9e
shreyakale08 <kaleshreya347@gmail.com>
2026-04-20 01:52:55 +0530

    Add new file

---
+++ Practice.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Palindrome{
+
+    public static void main(String[] args){
+
+        int num = 76867;
+        String str = "";
+
+        for(int i = str.length() - 1; i>=0; i--){
+            rev+=str.charAt(i)
+        }
+        if (str.equals(rev)){
+
+            System.out.println("Palindrome");
+        }
+        else{
+           System.out.println("Not Palindrome") 
+        }
+    }
+}
